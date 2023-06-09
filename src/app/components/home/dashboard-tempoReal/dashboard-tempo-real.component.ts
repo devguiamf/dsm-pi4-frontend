f55adc7d6b26ea813f2e6d443094126bf37c4702
devguiamf <guilherme.amf18@gmail.com>
2023-06-08 21:00:11 -0300

    feat: component socket

--- src/app/components/home/dashboard-tempoReal/dashboard-tempo-real.component.ts
+++ src/app/components/home/dashboard-tempoReal/dashboard-tempo-real.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import {DateAdapter, MAT_DATE_LOCALE} from '@angular/material/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard-tempo-real',
@@ -12,6 +13,12 @@ import {DateAdapter, MAT_DATE_LOCALE} from '@angular/material/core';
 export class DashboardTempoRealComponent implements OnInit {
 
   @Inject(MAT_DATE_LOCALE) private _locale: string | undefined = 'pt-BR'
+  @ViewChild('canva', { static: true }) element!: ElementRef;
+  chartJS!: Chart;
+  energyType!: string
+  moneyType!: string
+  hours!: string[]
+
   types = [
     {description: 'Energia', value:'1', icon:'electric_bolt', type: 'Khw' },
     {description: `Dinheiro`, value:'2', icon:'payments', type: 'R$'}
@@ -23,12 +30,56 @@ export class DashboardTempoRealComponent implements OnInit {
     private _adapter: DateAdapter<any>
   ){}
 
-
   ngOnInit(): void {
     this._adapter.setLocale(this._locale);
   }
 
-  typeSelect(event: any){
 
+  ngAfterContentInit(): void {
+    this.initChart();
+  }
+
+  private initChart() {
+    this.chartJS = new Chart(this.element.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['2','2'],
+        datasets: [
+          {
+            label: 'Dinheiro R$',
+            data: [],
+            borderWidth: 4,
+            borderColor: 'rgb(58, 148, 74)',
+            backgroundColor: 'white',
+            borderCapStyle: 'round',
+            fill: false,
+          },
+          {
+            label: 'Energia kWh',
+            data: [],
+            borderWidth: 4,
+            borderColor: 'yellow',
+            backgroundColor: 'white',
+            borderCapStyle: 'round',
+            fill: false,
+          },
+        ],
+      },
+      options: {
+        animation: {
+          duration: 500,
+          easing: 'linear',
+          loop: false,
+        },
+        layout: {
+          padding: {
+            left: 20,
+            bottom: 20,
+            right: 20,
+            top: 20,
+          },
+        },
+      },
+    });
   }
 }
